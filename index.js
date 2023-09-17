aa6fcb24bdeaaa610ed35a36686803080b756ea4
Taufiq <htaufiq225@gmail.com>
2023-09-17 09:56:48 -0700

    update index.js

--- index.js
+++ index.js
@@ -23,6 +23,13 @@ const store = new sessionStore({
 });
 
 app.use(express.static("public"));
+
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+);
 app.use(
   session({
     secret: process.env.SECRET,
@@ -34,12 +41,7 @@ app.use(
     },
   })
 );
-app.use(
-  cors({
-    credentials: true,
-    origin: "http://localhost:3000",
-  })
-);
+
 app.use(express.json());
 app.use(FileUpload());
 app.use(authRoute);
