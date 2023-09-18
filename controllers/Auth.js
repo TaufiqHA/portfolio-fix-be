1c7e171d9a05027d2201e23851851156282322bd
Taufiq <htaufiq225@gmail.com>
2023-09-17 20:56:13 -0700

    update auth.js

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -9,7 +9,8 @@ export const login = async (req, res) => {
   });
   if (!user) return res.status(404).json({ msg: "user tidak ditemukan" });
   const match = await argon2.verify(user.password, req.body.password);
-  if (!match) return res.status(400).json("password yang anda masukkan salah");
+  if (!match)
+    return res.status(400).json({ msg: "password yang anda masukkan salah" });
   req.session.user = user.uuid;
   const uuid = user.uuid;
   const email = user.email;
